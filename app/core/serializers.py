338d847262e5b55fd0b332448fa3ec0a4e1895a5
Donovan <donovansoft@gmail.com>
2023-03-31 20:07:25 -0600

    fix url redirect

--- app/core/serializers.py
+++ app/core/serializers.py
@@ -9,9 +9,3 @@ class ShortURLSerializer(serializers.ModelSerializer):
         model = ShortURL
         fields = ['id', 'original_url', 'short_code']
         read_only_fields = ('id', 'short_code')
-
-    def to_representation(self, instance):
-        """Return the short URL with the BASE_URL"""
-        data = super().to_representation(instance)
-        data['short_url'] = f"{settings.BASE_URL}/{instance.short_code}"
-        return data
